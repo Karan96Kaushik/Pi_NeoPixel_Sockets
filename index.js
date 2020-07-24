49958b1ed3926f8c6411607aecef73136b86740e
Karan Kaushik <karankaushik69@gmail.com>
2020-07-24 16:25:20 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server);
+const { spawn } = require('child_process');
+
+
+
 io.on('connection', client => {
 	console.log("Connected", (new Date()).toLocaleString())
-	client.on('event', data => { /* … */ });
+	client.on('event', data => {
+
+		console.log(data)
+
+		const python = spawn('python3', ['neo.py']);
+
+		python.stdout.on('data', function (data) {
+			console.log('Pipe data from python script ...');
+			dataToSend = data.toString();
+		});
+
+		python.on('close', (code) => {
+			console.log(`child process close all stdio with code ${code}`);
+		});
+	});
 	client.on('disconnect', () => {
 		console.log("Disconnected", (new Date()).toLocaleString())
 	});
 });
+
+
+
+
 server.listen(3000);
