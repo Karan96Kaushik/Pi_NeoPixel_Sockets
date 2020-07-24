7d6135615c731a6de7406c2c7bbb9207b5dd1425
Karan Kaushik <karankaushik69@gmail.com>
2020-07-24 16:52:37 +0530

    update

--- index.js
+++ index.js
@@ -2,26 +2,33 @@ const server = require('http').createServer();
 const io = require('socket.io')(server);
 const { spawn } = require('child_process');
 
-
-
 io.on('connection', client => {
 	console.log("Connected", (new Date()).toLocaleString())
-	client.on('event', data => {
+	
+	client.on('single', color => {
 
-		console.log(data)
+		// console.log(data)
 
-		const python = spawn('python3', ['neo_single.py', data]);
+		const python = spawn('python3', ['neo_single.py', color]);
 
 		python.stdout.on('data', function (data) {
-			console.log('Pipe data from python script ...');
+			// console.log('Pipe data from python script ...');
 			dataToSend = data.toString();
-			console.log(dataToSend);
-		});
-
-		python.on('close', (code) => {
-			console.log(`child process close all stdio with code ${code}`);
+			// console.log(dataToSend);
 		});
 	});
+
+	client.on('rainbow', color => {
+
+		const python = spawn('python3', ['neo_rainbow.py']);
+
+		python.stdout.on('data', function (data) {
+			// console.log('Pipe data from python script ...');
+			dataToSend = data.toString();
+			// console.log(dataToSend);
+		});
+	});
+	
 	client.on('disconnect', () => {
 		console.log("Disconnected", (new Date()).toLocaleString())
 	});
