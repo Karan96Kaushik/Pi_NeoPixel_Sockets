35c2fe28baa5a7f4ab37d69b610b141ad00cc44d
Karan Kaushik <karankaushik69@gmail.com>
2020-07-24 16:41:23 +0530

    update

---
+++ neo_single.py
@@ -0,0 +1,30 @@
+import time
+import board
+import neopixel
+import sys
+
+print(sys.argv)
+rgb = sys.argv[1]
+print(rgb[1:3])
+print(rgb[3:5])
+print(rgb[5:7])
+ 
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+ 
+# The number of NeoPixels
+num_pixels = 1
+ 
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+ 
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+pixels.fill((255, 0, 0))
+    # Uncomment this line if you have RGBW/GRBW NeoPixels
+    # pixels.fill((255, 0, 0, 0))
+pixels.show()
