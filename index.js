24d1fb18c1ec350f39b62129b5b6fd08dfa6f538
Karan Kaushik <karankaushik69@gmail.com>
2020-07-24 17:05:29 +0530

    update

--- index.js
+++ index.js
@@ -4,37 +4,38 @@ const { spawn } = require('child_process');
 
 io.on('connection', client => {
 	console.log("Connected", (new Date()).toLocaleString())
+	update_neo('neo_single.py', '#00ff00')
+	setTimeout(() => {
+		update_neo('neo_single.py', '#000000')		
+	}, 1500);
 	
 	client.on('single', color => {
-
-		// console.log(data)
-
-		const python = spawn('python3', ['neo_single.py', color]);
-
-		python.stdout.on('data', function (data) {
-			// console.log('Pipe data from python script ...');
-			dataToSend = data.toString();
-			// console.log(dataToSend);
-		});
+		update_neo('neo_single.py', color)
 	});
 
 	client.on('rainbow', color => {
-
-		const python = spawn('python3', ['neo_rainbow.py']);
-
-		python.stdout.on('data', function (data) {
-			// console.log('Pipe data from python script ...');
-			dataToSend = data.toString();
-			// console.log(dataToSend);
-		});
+		update_neo('neo_rainbow.py')
 	});
 	
 	client.on('disconnect', () => {
+		update_neo('neo_single.py', '#ff0000')
+		setTimeout(() => {
+			update_neo('neo_single.py', '#000000')		
+		}, 1500);
 		console.log("Disconnected", (new Date()).toLocaleString())
 	});
 });
 
 
+function update_neo(state, color) {
+	const python = spawn('python3', [state, color]);
+
+		python.stdout.on('data', function (data) {
+			// console.log('Pipe data from python script ...');
+			dataToSend = data.toString();
+			// console.log(dataToSend);
+		});
+} 
 
 
 server.listen(3000);
